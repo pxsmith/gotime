dc8afab5735491d40fa695293b82c92d029d3687
pxsmith <thephilsmith@icloud.com>
2024-02-03 11:52:47 -0500

    ✨ Animations on scroll

--- source/scripts.js
+++ source/scripts.js
@@ -1,42 +1,14 @@
-// document.addEventListener('scroll', function() {
-//   var element = document.querySelector('.fade-in');
-//   var position = element.getBoundingClientRect();
-
-//   // If the top of the element is in the viewport, add the 'active' class
-//   if (position.top < window.innerHeight && position.bottom >= 0) {
-//     element.classList.add('active');
-//   }
-// });
-
-document.addEventListener('scroll', function() {
-  $('.fade-in').each(function() {
-    var element = $(this);
-    var position = element.offset().top;
-
-    // If the top of the element is in the viewport, add the 'active' class
-    if (position < window.innerHeight + window.scrollY) {
-      element.addClass('active');
-    }
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry)
+    if (entry.isIntersecting) {
+      entry.target.classList.add('animation--show');
+    } 
+    // else {
+    //   entry.target.classList.remove('animation--show');
+    // }
   });
 });
 
-
-
-// Perplexity written code
-
-// window.addEventListener('scroll', function() {
-//   if (window.pageYOffset > 100) {
-//     const box = document.querySelector('.box');
-//     box.style.animation = 'move 2s linear both';
-//   }
-// });
-
-// const box = document.querySelector('.box');
-// const observer = new IntersectionObserver((entries) => {
-//   if (entries[0].isIntersecting) {
-//     box.style.animation = 'move 2s linear both';
-//     observer.unobserve(box);
-// }
-// });
-
-// observer.observe(box);
+const hiddenElements = document.querySelectorAll('.animate--on-scroll');
+hiddenElements.forEach((el) => observer.observe(el));
